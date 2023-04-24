da1bccc7b6d8954babb52636c7dc11d402ee84d9
Dahye Kim <digeuthi@gmail.com>
2023-04-24 11:10:03 +0900

    jpa study sql

---
+++ firstproject/src/main/java/com/dahye/firstproject/entity/ExampleEntity.java
@@ -0,0 +1,7 @@
+package com.dahye.firstproject.entity;
+
+public class ExampleEntity {
+    private int example_column1;
+    private String example_column2;
+    private boolean example_column3;
+}
