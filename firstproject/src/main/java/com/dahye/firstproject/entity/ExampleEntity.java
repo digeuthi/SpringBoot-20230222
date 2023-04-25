a6dceddaafe63a09d5324bd6b0a9fbafd431040f
Dahye Kim <digeuthi@gmail.com>
2023-04-25 09:53:53 +0900

    repository query method

--- firstproject/src/main/java/com/dahye/firstproject/entity/ExampleEntity.java
+++ firstproject/src/main/java/com/dahye/firstproject/entity/ExampleEntity.java
@@ -8,6 +8,7 @@ import javax.persistence.Id;
 import javax.persistence.Table; //어느 경로 어노테이션인지 알기!
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -15,18 +16,20 @@ import lombok.NoArgsConstructor;
 //세터를 가지면 안된다는 이야기(빈생성자는 없어야함) DB와 인스턴스가 데이터를 꺼내오고나서 set작업을 하면 잘못전달될수 있다는것.
 //그런 것에 제약을 걸어놓기 위해 setter쓰지말라고하지만 set 안적으면 좀 불편하다. 써도 무관하긴하다..
 @Data
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity(name="Example") //name을 이 이름으로 사용할수 있게 지정, DB에서는 해당테이블 못찾는다..! 테이블 어노테이션 추가
-@Table(name="Example") //테이블 찾아오게 함
+@Table(name="Example") //테이블 찾아오게 함 //어떠한 테이블과 매핑할건지 명시하게 된다.
 //DB의 여러 제약조건에서 기본키는 만드시 있어야한다! -> 지정하는 어노테이션이 따로 있다.
 //java에서는 보통 카멜케이스로 네이밍규칙. 띄어쓰기다음에 오면 대문자로 함 / db에서는 띄어쓰기 _로하고 소문자로해야 서로 인식함!
 public class ExampleEntity { 
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //autoInclement 적용시킨다
     //다른 제약조건을 설정하려면 @Column사용해서 지정함
-    @Column(name="example_column1", nullable = false, unique = true)
+    @Column(name="example_column1", nullable = false, unique = true) //column 사용하지 않으면 명명규칙 엄격하게 따라야한다.
+    //클래스도 어노테이션 지정하지 않고 할 경우 명명규칙 정확히 따라야한다
     private int pk; 
     private String exampleColumn2;
     private boolean exampleColumn3;
--- firstproject/src/main/java/com/dahye/firstproject/service/implement/RestApiServiceImplement.java
+++ firstproject/src/main/java/com/dahye/firstproject/service/implement/RestApiServiceImplement.java
@@ -1,25 +1,50 @@
 package com.dahye.firstproject.service.implement;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
+import com.dahye.firstproject.entity.ExampleEntity;
+import com.dahye.firstproject.repository.ExampleRepository;
 import com.dahye.firstproject.service.RestAPIService;
 
 //@Component //Ioc 이용한 DI사용가능하다
 @Service // Component와 동일한데 이름을 정확하게 지정해주는것
 public class RestApiServiceImplement implements RestAPIService { //구현체
     
-    public String getMethod(){
-        return "Return to Service Rayer";
+    private ExampleRepository exampleRepository;
+
+    @Autowired
+    public RestApiServiceImplement(ExampleRepository exampleRepository){
+        this.exampleRepository = exampleRepository;
     }
 
     public String postMethod(){
-        return "Return to Service Rayer";
+        // 데이터 삽입
+        // 1. Entity 인스턴스(= 데이터 베이스 테이블의 레코드) 생성 
+        ExampleEntity exampleEntity = 
+            ExampleEntity.builder()
+            .exampleColumn2("string1")
+            .exampleColumn3(false)
+            .build();
+
+        // 2. Repository를 거쳐서 Entity 인스턴스를 저장
+        exampleRepository.save(exampleEntity);
+
+        return null;
     }
+
+    public String getMethod(){
+        //데이터 조회
+        // 1. jpaRepository에 있는 findBy 메서드로 Entity 조회
+        ExampleEntity exampleEntity = exampleRepository.findById(1).get();
+        return exampleEntity.toString(); //반환을 String으로 설정해뒀으니까
+    }
+
     public String patchMethod(){
-        return "Return to Service Rayer";
+        return null;
     }
     public String deleteMethod(){
-        return "Return to Service Rayer";
+        return null;
     }
 }
