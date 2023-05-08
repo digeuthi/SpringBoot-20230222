2a1fd328d80df4581ad94c1803eb6c13a2789b4a
Dahye Kim <digeuthi@gmail.com>
2023-05-08 11:28:28 +0900

    board api study

--- Board/src/main/java/com/dahye/board/config/WebSecurityConfig.java
+++ Board/src/main/java/com/dahye/board/config/WebSecurityConfig.java
@@ -1,5 +1,11 @@
 package com.dahye.board.config;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,12 +13,26 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import com.dahye.board.filter.JwtAuthenticationFilter;
 
 import io.jsonwebtoken.Jwt;
+
+class FailedAuthenticationEntiryPoint implements AuthenticationEntryPoint{
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response,
+            AuthenticationException authException) throws IOException, ServletException {
+        response.setContentType("application/json");
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.getWriter().write("{\"code\": \"AF\",\"message\" : \"Authentication failed\"}"); // \를 이스케이프 문자를 문자열로 받기위해서 작성
+    }
+    
+}
 
 @EnableWebSecurity
 @Configuration
@@ -34,7 +54,8 @@ public class WebSecurityConfig {
                     .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                     .authorizeHttpRequests().antMatchers("/api/v1/**","/api/v2/auth/**","/api/v2/board/list","/api/v2/board/top3").permitAll()
                     .antMatchers(HttpMethod.GET,"/api/v2/board/*").permitAll()
-                    .anyRequest().authenticated();
+                    .anyRequest().authenticated().and()
+                    .exceptionHandling().authenticationEntryPoint(new FailedAuthenticationEntiryPoint());
 
         httpSecurity.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
